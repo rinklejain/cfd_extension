8890033482058cb7f1e539c185b153e64b3a866e
Rinkle Jain <rinklejain96@gmail.com>
2017-02-05 19:21:32 +0530

    changes to bg.js

--- bg.js
+++ bg.js
@@ -1,33 +1,45 @@
 function get_contests(){
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "http://codeforces.com/api/contest.list", true);
-xmlhttp.send();
 var upcoming_count = 0;
 var ongoing_count = 0;
 var upcoming_contests = {items: []};
 var ongoing_contests = {items: []};
+console.log("rinkle ka infinite swag");
 xmlhttp.onreadystatechange = function() {
+  console.dir(this);
     if (this.readyState == 4 && this.status == 200) {
-	var new_contest_count = 0;
-        var contest_list = JSON.parse(this.responseText);	
-	var result = contest_list;
-	console.log(result[0]);
-	for(var i=0; i<result.length; i++)
+      //document.getElementById("aa").innerHTML = contest_list[];
+      var contest_list = JSON.parse(this.responseText);	
+      document.getElementById("aa").innerHTML = contest_list[0].id
+	    console.log(contest_list[0]);
+	for(var i=0; i<contest_list.length; i++)
 	{
-	  var output= check_status(result[i].startTimeSeconds, result[i].startTimeSeconds+result[i].durationSeconds);
+	  var output= check_status(contest_list[i].startTimeSeconds, contest_list[i].startTimeSeconds+contest_list[i].durationSeconds);
 	  if(output == 1){
-	    ongoing_contests.items.push({"id": result[i].id,
-                                         "name": result[i].name, 
-                                         "st": result[i].startTimeSeconds, 
-                                         "du": result[i].durationSeconds, 
-                                         "url": "http://codeforces.com/contest/"+result[i].id
+	    ongoing_contests.items.push({"id": contest_list[i].id,
+                                         "name": contest_list[i].name, 
+                                         "st": contest_list[i].startTimeSeconds, 
+                                         "du": contest_list[i].durationSeconds, 
+                                         "url": "http://codeforces.com/contest/"+contest_list[i].id
                                        });
 	  }
-	}
-        console.log(ongoing_contests);
+    if(output == 2){
+      upcoming_contests.items.push({"id": contest_list[i].id,
+                                         "name": contest_list[i].name, 
+                                         "st": contest_list[i].startTimeSeconds, 
+                                         "du": contest_list[i].durationSeconds, 
+                                         "url": "http://codeforces.com/contest/"+contest_list[i].id
+                                       });
     }
+	}
+        console.log(upcoming_contests[0].id);
+  }
 };
+xmlhttp.open("GET", "http://codeforces.com/api/contest.list", true);
+xmlhttp.send();
+
 }
+
 
 function check_status(start_time, end_time){
   var current_date = new Date();
@@ -43,5 +55,6 @@ function check_status(start_time, end_time){
   }
 }
 
+get_contests();
 //setInterval(get_contests(),1800000);
 
